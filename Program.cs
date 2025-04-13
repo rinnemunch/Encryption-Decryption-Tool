cbd16b3432040b66d96e83527f7e60302819fdd1
Shaun Fulton <fulton_shaun@yahoo.com>
2025-04-13 06:26:24 -0400

    Shifting the letters

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿Console.Write("Enter a message to encrypt: ");   
+﻿int shift = 3; // Shift value for Caesar cipher 
+string encrypted = "";
+
+
+
+Console.Write("Enter a message to encrypt: ");   
 string input = Console.ReadLine() ?? "";
+
+foreach (char c in input)
+{
+    if (char.IsLetter(c))
+    {
+        char offset = char.IsUpper(c) ? 'A' : 'a';
+        char shifted = (char)((((c + shift) - offset) % 26) + offset);
+        encrypted += shifted;
+    }
+    else
+    {
+        encrypted += c;
+    }
+}
